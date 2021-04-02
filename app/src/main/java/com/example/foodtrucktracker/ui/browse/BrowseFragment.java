b42494b8f2f195677bf51bf85029ebd42dc366f6
ConnorDuncan <dnpawe@gmail.com>
2021-04-02 15:45:29 -0700

    Update and rename app/src/main/java/com/example/foodtrucktracker/ui/home/HomeFragment.java to app/src/main/java/com/example/foodtrucktracker/ui/browse/BrowseFragment.java

--- app/src/main/java/com/example/foodtrucktracker/ui/browse/BrowseFragment.java
+++ app/src/main/java/com/example/foodtrucktracker/ui/browse/BrowseFragment.java
@@ -1,4 +1,4 @@
-package com.example.foodtrucktracker.ui.home;
+package com.example.foodtrucktracker.ui.browse;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.foodtrucktracker.R;
 
-public class HomeFragment extends Fragment {
+public class BrowseFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private BrowseViewModel browseViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        browseViewModel =
+                new ViewModelProvider(this).get(BrowseViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_browse, container, false);
+        final TextView textView = root.findViewById(R.id.text_browse);
+        browseViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
